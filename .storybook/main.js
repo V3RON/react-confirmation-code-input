8a522b4ee06c5b2788bb77c1c26b7958167d0c34
Szymon Chmal <szymon@chmal.me>
2020-12-01 23:45:41 +0100

    feat: add default styles for the input

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,23 +6,5 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ],
-  webpackFinal: (config) => {
-    config.module.rules = config.module.rules.filter((f) => f.test.toString() !== '/\\.css$/');
-
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,
-          },
-        },
-      ],
-    });
-
-    return config;
-  }
+  ]
 }
